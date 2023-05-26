e63bfbced81b9a49142ee7c54f4026ef84476aab
Shaid Sifat <shaidsifat@Shaids-MacBook-Pro.local>
2023-05-26 19:18:50 +0600

    added list

--- Emails/views.py
+++ Emails/views.py
@@ -16,23 +16,10 @@ class SendEmal(APIView):
 
 
     def get(self,request,format=None):
-
-    
-        # snippets = Email.objects.all()
-        #  serializer =EmailSerializer(snippets, many=True)
-            subject = 'New message send'
-            message = f'A new meesage for interactivecare.'
-            from_email = settings.DEFAULT_FROM_EMAIL
-            recipient_list = ['shaidsifat55@gmail.com','sifat15-7616@diu.edu.bd',] # Replace this with the email address where you want to receive the notification
-            
-            res=send_bulk_email(subject, message, from_email, recipient_list)
-           # print(res)
-            if(res == 1):  
-                msg = "Mail Sent Successfuly"  
-            else:  
-                msg = "Mail could not sent"  
-            return HttpResponse(msg)  
-                
+        query = Email.objects.all().values('id','recipients','subject','message')
+        if query:
+          return Response({"message":query}, status=status.HTTP_200_OK)
+        return Response({"message":"data not found"})
 
 
 
@@ -43,15 +30,7 @@ class SendEmal(APIView):
             serializer.save()  
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-# views.py
 
 
 
-def send_bulk_emails_view(request):
-    recipients = ['email1@example.com', 'email2@example.com', 'email3@example.com']
-    subject = 'Hello'
-    message = 'This is a bulk email message.'
 
-    send_bulk_email(recipients, subject, message)
-
-    return render(request, 'email_sent.html')
